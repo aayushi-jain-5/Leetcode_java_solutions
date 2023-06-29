f7e13293b1afd574695b4a4a0b7aa758a3de405a
Aayushi Jain <101443827+aayushi-jain-5@users.noreply.github.com>
2023-06-29 10:30:06 +0530

    Create Palindrome_num.java

---
+++ Easy/Palindrome_num.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        final int N = nums.length;
+        int[] A = new int[2];
+        for(int i=0;i<N; i++){  // Valid answer must be there before end of loop
+            for(int j=i+1; j < N; j++){
+                if(nums[i] + nums[j] == target){
+                    A[0] = i; A[1] = j;
+                    return A;
+                }
+            }
+        }
+        return A;
+    }
+}
