77af01dd888de8cc811f49d370a2445c4da3dcbf
Aayushi Jain <101443827+aayushi-jain-5@users.noreply.github.com>
2023-07-03 22:16:42 +0530

    Create Path Sum.java

---
+++ Easy/Path Sum.java
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        if(root ==null) return false;
+
+        boolean left = false;
+        boolean right = false;
+
+        targetSum-=root.val;
+        if(root.left==null && root.right==null && targetSum==0){
+            return true;
+        }
+        if(root.left!=null){
+            left = hasPathSum(root.left , targetSum);
+            // return left;
+        }
+        if(root.right!=null){
+            right = hasPathSum(root.right , targetSum);
+            // return right;
+        }
+        return left || right;
+    }
+}
