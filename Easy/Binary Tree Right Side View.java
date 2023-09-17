3872535bfb243551abd3cac050c1d45bc996d261
Aayushi Jain <101443827+aayushi-jain-5@users.noreply.github.com>
2023-09-17 14:22:50 +0530

    Create Binary Tree Right Side View.java

---
+++ Easy/Binary Tree Right Side View.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int maxLevel = 0;
+    List<Integer> list = new ArrayList();
+    public List<Integer> rightSideView(TreeNode root) {
+        if(root == null) return list;
+        rightView(root,1);
+        return list;
+    }
+    void rightView(TreeNode root,int level){
+        if(root == null) return;
+        if(maxLevel < level){
+            list.add(root.val);
+            maxLevel = level;
+        }
+        rightView(root.right,level+1);
+        rightView(root.left,level+1);
+    }
+}
